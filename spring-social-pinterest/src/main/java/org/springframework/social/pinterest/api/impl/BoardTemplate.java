925b2e136f315b21c6060879df32072c9f9cc058
Rajaboopathy Vijayaraghavan <rajaboopathy.vijayaraghavan@capitalone.com>
2016-02-20 17:01:35 -0600

    Completed Board Operations

--- spring-social-pinterest/src/main/java/org/springframework/social/pinterest/api/impl/BoardTemplate.java
+++ spring-social-pinterest/src/main/java/org/springframework/social/pinterest/api/impl/BoardTemplate.java
@@ -1,6 +1,8 @@
 package org.springframework.social.pinterest.api.impl;
 
 import org.springframework.social.pinterest.api.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -18,26 +20,32 @@ public class BoardTemplate implements BoardOperations {
 
     @Override
     public Board createBoards(String name, String description) {
-        return null;
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("name", name);
+        map.add("description", description);
+        return pinterestApi.post("v1/Boards/", map, Board.class);
     }
 
     @Override
     public Board editBoards(String userName, String boardName, String name, String description) {
-        return null;
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("name", name);
+        map.add("description", description);
+        return pinterestApi.patch("v1/Boards/", "/" + userName + "/" + boardName + "/", map, Board.class);
     }
 
     @Override
     public void deleteBoards(String userName, String boardName) {
-
+        pinterestApi.delete("v1/Boards/", "/" + userName + "/" + boardName + "/");
     }
 
     @Override
     public Board getUserBoards(String userName, String boardName) {
-        return null;
+        return pinterestApi.fetchObject("v1/Boards/" + userName + "/" + boardName + "/", Board.class);
     }
 
     @Override
     public PagedList<Pin> getUserBoardPins(String userName, String boardName) {
-        return null;
+        return pinterestApi.fetchListOfObject("v1/Boards/" + userName + "/" + boardName + "/", null, Pin.class);
     }
 }
