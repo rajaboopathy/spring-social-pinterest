7efaf8454aadaf22fceaef1deba2674225607245
Rajaboopathy Vijayaraghavan <rajaboopathy.vijayaraghavan@capitalone.com>
2016-02-20 17:13:13 -0600

    Complete pin operations

--- spring-social-pinterest/src/main/java/org/springframework/social/pinterest/api/PinOperations.java
+++ spring-social-pinterest/src/main/java/org/springframework/social/pinterest/api/PinOperations.java
@@ -4,4 +4,10 @@ package org.springframework.social.pinterest.api;
  * Created by Rajaboopathy Vijay on 11/15/15.
  */
 public interface PinOperations {
+
+    Pin getPin(String pin);
+
+    Pin editPin(String pin, String board, String username, String note, String link);
+
+    void deletePin(String pin);
 }
--- spring-social-pinterest/src/main/java/org/springframework/social/pinterest/api/impl/PinTemplate.java
+++ spring-social-pinterest/src/main/java/org/springframework/social/pinterest/api/impl/PinTemplate.java
@@ -1,9 +1,41 @@
 package org.springframework.social.pinterest.api.impl;
 
+import org.springframework.social.pinterest.api.Pin;
 import org.springframework.social.pinterest.api.PinOperations;
+import org.springframework.social.pinterest.api.PinterestApi;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Rajaboopathy Vijay on 11/15/15.
  */
 public class PinTemplate implements PinOperations {
+
+    private PinterestApi pinterestApi;
+
+    private RestTemplate restTemplate;
+
+    public PinTemplate(PinterestApi pinterestApi, RestTemplate restTemplate) {
+        this.pinterestApi = pinterestApi;
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public Pin getPin(String pin) {
+        return pinterestApi.fetchObject("/v1/pins/" + pin, Pin.class);
+    }
+
+    @Override
+    public Pin editPin(String pin, String board, String username, String note, String link) {
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("note", note);
+        map.add("link", link);
+        return pinterestApi.patch("/v1/pins/" + pin, "/" + username + "/" + board + "/", null, Pin.class);
+    }
+
+    @Override
+    public void deletePin(String pin) {
+        pinterestApi.delete("/v1/pins/" + pin);
+    }
 }
